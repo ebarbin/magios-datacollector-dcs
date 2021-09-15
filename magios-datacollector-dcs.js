2ca1efbd06274d21ad7140dfd2ea15414c88a6e1
ebarbin <ebarbin@geocom.com.uy>
2021-09-15 14:54:49 -0300

    post changes

--- magios-datacollector-dcs.js
+++ magios-datacollector-dcs.js
@@ -3,7 +3,8 @@ const readLastLines = require('read-last-lines');
 const axios = require('axios');
 
 const DCS_LOG_FILE_PATH = 'dcs.log';
-const BOT_URL = 'https://magios-datacollector-bot.herokuapp.com/';
+const BOT_URL = 'https://magios-datacollector-bot.herokuapp.com';
+const SERVER_ID = '2';
 
 console.log('magios-datacollector-dcs has started!!');
 
@@ -14,17 +15,32 @@ watch(DCS_LOG_FILE_PATH, { recursive: true }, function(evt, name) {
 
             if (line.indexOf('event:type=birth') >= 0) {
 
-                if (line.split(',').find(value => value.indexOf('initiatorPilotName=') >= 0)) {
-                    const username = line.split(',').find(value => value.indexOf('initiatorPilotName=') >= 0).split('=')[1];
+                const lineArray = line.split(',');
+                const strDate = lineArray[0].split('INFO')[0].trim();
 
-                    axios.get(BOT_URL + username).then(function (response) {
-                        console.log('Message sent - Username:' + username + ' has connected.');
-                    }).catch(function (error) {
-                        console.log('Error: ' + error);
-                    });
-                }
+                const username = getValue(lineArray, 'initiatorPilotName=');
+
+                axios.post(BOT_URL + '/user-join-server', {
+                    username: username,
+                    date: strDate,
+                    serverId: SERVER_ID
+                  }).then(function (response) {
+                    console.log('Message sent - Username:' + username + ' has connected.');
+                  }).catch(function (error) {
+                    console.log(error);
+                  });
             }
         });
 });
+
+getValue = (array, key) => {
+    const value = array.find(value => value.indexOf(key) >= 0);
+    if (value) {
+        return value.split('=')[1]
+    } else {
+        return null;
+    }
+}
+
 //Comando para generar el ejecutable:
 //pkg .\magios-datacollector-dcs.js --targets node10-win-x64 --targets node10-win-x64
